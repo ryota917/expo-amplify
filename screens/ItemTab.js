4493c9d7842a93054d73d79ea0a626d4107db479
sasataniryota <61574281+sasataniryota@users.noreply.github.com>
2020-10-18 15:15:09 +0900

    無限ローディング実装

--- screens/ItemTab.js
+++ screens/ItemTab.js
@@ -20,7 +20,8 @@ export default class ItemTab extends React.Component {
             searchCondition: [{color: ''}, {size: ''}, {season: ''}],
             items: [],
             nextToken: '',
-            hasMoreItems: true
+            canLoad: true,
+            isLoading: false
         }
     }
 
@@ -30,13 +31,14 @@ export default class ItemTab extends React.Component {
         headerRight:() => <Icon name='search' size={24} onPress={() => {navigation.navigate('SearchConditionModal')}} style={{paddingRight: 20}}/>
     });
 
-    componentDidMount() {
+    componentDidMount = async () => {
         this.syncUserAndCartToDynamo();
         //navigationのイベントリスナーでTabが押された時に毎回アイテム情報を取得する
-        this.props.navigation.addListener('didFocus', async () => {
+        await this.props.navigation.addListener('didFocus', async () => {
             //stateが更新されるのとawaitしないと前のstateで表示される
             await this.updateSearchState()
-            this.fetchItemsInf()
+            //this.fetchItemsInf()
+            this.flatLoad()
         })
     }
 
@@ -74,75 +76,11 @@ export default class ItemTab extends React.Component {
         }
     }
 
-    //検索クエリ生成
-    searchQuery = () => {
-        const condition = this.state.searchCondition.filter(ele => Object.values(ele)[0].length )
-        console.log(condition)
-        const limitNum = condition.length
-        console.log(limitNum)
-        switch(limitNum) {
-            case 3:
-                console.log('3つ')
-                return {
-                    filter: {
-                        and: {
-                            and: {
-                                season: {
-                                    eq: condition[2].season
-                                }
-                            },
-                            size: {
-                                eq: condition[1].size
-                            }
-                        },
-                        color: {
-                            eq: condition[0].color
-                        }
-                    }
-                }
-                break;
-            case 2:
-                console.log('2つ')
-                const key1 = Object.keys(condition[0])
-                const key2 = Object.keys(condition[1])
-                return {
-                    filter: {
-                        and: {
-                            [key1]: {
-                                eq: condition[0][key1]
-                            }
-                        },
-                        [key2]: {
-                            eq: condition[1][key2]
-                        }
-                    }
-                }
-                break;
-            case 1:
-                console.log('1つ')
-                const key = Object.keys(condition[0])
-                return {
-                    filter: {
-                        [key]: {
-                            eq: condition[0][key]
-                        }
-                    }
-                }
-                break;
-            case 0:
-                console.log('デフォルトです')
-                return {}
-        }
-    }
-
     loadSearchQuery = () => {
         const condition = this.state.searchCondition.filter(ele => Object.values(ele)[0].length )
-        console.log(condition)
         const limitNum = condition.length
-        console.log(limitNum)
         switch(limitNum) {
             case 3:
-                console.log('3つ')
                 return {
                     filter: {
                         and: {
@@ -164,7 +102,6 @@ export default class ItemTab extends React.Component {
                 }
                 break;
             case 2:
-                console.log('2つ')
                 const key1 = Object.keys(condition[0])
                 const key2 = Object.keys(condition[1])
                 return {
@@ -183,7 +120,6 @@ export default class ItemTab extends React.Component {
                 }
                 break;
             case 1:
-                console.log('1つ')
                 const key = Object.keys(condition[0])
                 return {
                     filter: {
@@ -196,19 +132,15 @@ export default class ItemTab extends React.Component {
                 }
                 break;
             case 0:
-                console.log('デフォルトです')
-                return { limit: 3, nextToken: this.state.nextToken }
+                return { limit: 10, nextToken: this.state.nextToken }
         }
     }
 
     fetchSearchQuery = () => {
         const condition = this.state.searchCondition.filter(ele => Object.values(ele)[0].length )
-        console.log(condition)
         const limitNum = condition.length
-        console.log(limitNum)
         switch(limitNum) {
             case 3:
-                console.log('3つ')
                 return {
                     filter: {
                         and: {
@@ -229,7 +161,6 @@ export default class ItemTab extends React.Component {
                 }
                 break;
             case 2:
-                console.log('2つ')
                 const key1 = Object.keys(condition[0])
                 const key2 = Object.keys(condition[1])
                 return {
@@ -247,7 +178,6 @@ export default class ItemTab extends React.Component {
                 }
                 break;
             case 1:
-                console.log('1つ')
                 const key = Object.keys(condition[0])
                 return {
                     filter: {
@@ -259,101 +189,66 @@ export default class ItemTab extends React.Component {
                 }
                 break;
             case 0:
-                console.log('デフォルトです')
-                return { limit: 3 }
+                return { limit: 10 }
         }
     }
 
-    //propsでアイテム情報が渡ってきた場合はそれをstateにそれ以外の時は全てのアイテムを取得する
-    fetchItems = async () => {
-        const query = this.searchQuery()
-        console.log(query)
-        try {
-            const res = await API.graphql(graphqlOperation(gqlQueries.searchItems, query))
-            console.log(res)
-            this.setState({
-                items: res.data.searchItems.items,
-            })
-        } catch(e) {
-            console.log(e);
-        }
-    }
-
-    //Infinite最初のロード(nextTokenを含まない)
-    fetchItemsInf = async () => {
-        const query = this.fetchSearchQuery()
-        console.log(query)
-        try {
-            const res = await API.graphql(graphqlOperation(gqlQueries.searchItems, query))
-            console.log(res)
-            //nextTokenが1なら終了
-            if(res.data.searchItems.nextToken === '1') {
-                this.setState({ hasMoreItems: false })
-            }
-            //追加するDOMの生成
-            const pushItems = []
-            res.data.searchItems.items.map((ite, i) => {
-                pushItems.push(
-                    <View>
-                        <Text>item</Text>
-                        <Image source={ite.imag_url} style={{ width: 100, height: 100 }}/>
-                    </View>
-                )
-            })
-            console.log(pushItems)
-            const newItems = this.state.items.concat(pushItems)
-            console.log('fetch時のnewItemです')
-            console.log(newItems)
-            //stateに追加
-            this.setState({
-                items: newItems,
-                nextToken: res.data.searchItems.nextToken
-            })
-        } catch(e) {
-            console.log(e);
-        }
-    }
-
-    //ローディング時のアイテム取得
-    loadItems = async () => {
-        const query = this.loadSearchQuery()
-        console.log(query)
+    flatLoad = async () => {
+        console.log('flatLoad 開始')
+        const query = await this.fetchSearchQuery()
         const res = await API.graphql(graphqlOperation(gqlQueries.searchItems, query))
         console.log(res)
-        //nextTokenが1なら終了
-        if(res.data.searchItems.nextToken === '1') {
-            this.setState({ hasMoreItems: false })
-        }
-        //追加するDOMの生成
-        const pushItems = []
-        res.data.searchItems.items.map((ite, i) => {
-            pushItems.push(
-                <View>
-                    <Text>item</Text>
-                    <Image source={ite.imag_url} style={{ width: 100, height: 100 }}/>
-                </View>
-            )
+        const canLoad = !!(res.data.searchItems.nextToken)
+        await this.setState({
+            items: res.data.searchItems.items,
+            nextToken: res.data.searchItems.nextToken,
+            canLoad: canLoad
         })
-        const newItems = this.state.items.concat(pushItems)
-        console.log('load時のnewItemdesu')
-        console.log(newItems)
-        //stateに追加
-        this.setState({
-            items: newItems,
-            nextToken: res.data.searchItems.nextToken
-        })
+    }
+
+    startLoading = async () => {
+        console.log('ローディング開始')
+        this.setState({ isLoading: true })
+        const query = this.loadSearchQuery()
+        const res = await API.graphql(graphqlOperation(gqlQueries.searchItems, query))
+        console.log(res)
+        const canLoad = !!(res.data.searchItems.nextToken)
+        await this.setState(prevState => ({
+            items: prevState.items.concat(res.data.searchItems.items),
+            nextToken: res.data.searchItems.nextToken,
+            canLoad: canLoad,
+            isLoading: false
+        }))
     }
 
     render() {
+        const activityIndicator = <ActivityIndicator animating size='large'/>
+        const { canLoad, items, isLoading } = this.state
         return (
+                <FlatList
+                    //onRefresh={() => {}}
+                    data={items}
+                    renderItem={({ item }) => (
+                        <View>
+                            <Text>{item.id} {item.name}</Text>
+                            <Image source={{ uri: item.image_url }} style={{ width: 300, height: 300 }}/>
+                        </View>
+                    )}
+                    onEndReached={(canLoad && !isLoading) ? this.startLoading : null}
+                    onEndReachedThreshold={1}
+                    ListFooterComponent={canLoad ? activityIndicator : null}
+                />
+            /*
+            <ScrollView style={{ flex: 1 }}>
             <InfiniteScroll
                 pageStart={0}
-                loadMore={this.loadItems()}
+                loadMore={this.loadItems.bind(this)}
                 hasMore={this.state.hasMoreItems}
                 loader={<div className='loader' key={0}>Loading ...</div>}
             >
                 {this.state.items}
             </InfiniteScroll>
+            </ScrollView>
             //iphoneXにも対応するViewの生成
             //<SafeAreaView>
             /*
